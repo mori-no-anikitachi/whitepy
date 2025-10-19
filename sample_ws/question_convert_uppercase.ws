6190060c5e8fcc3f086b292417470d0a1f9bdf5c
Gitefu <out.grow.1028@gmail.com>
2025-10-19 19:19:28 +0900

    parseエラーを修正

---
+++ sample_ws/question_convert_uppercase.ws
@@ -0,0 +1,9 @@
+[stack]push#0   
+[I/O]	
+readc	 [stack]push#0   
+[heap]retrieve			[stack]push#32   	     
+[arithmetic]sub	  	[I/O]	
+printc  
+end0
+end1
+end2
--- whitepy/parser.py
+++ whitepy/parser.py
@@ -17,54 +17,66 @@ class Parser(object):
         self.instruction_ptr = 0
         self.call_ptr = []
         self.method_map = {
-            'STACK_MANIPULATION': {
-                'PUSH': partial(self.stack.push),
-                'DUP': self.stack.dup,
-                'SWAP': self.stack.swap,
-                'POP': self.stack.pop
+            "STACK_MANIPULATION": {
+                "PUSH": partial(self.stack.push),
+                "DUP": self.stack.dup,
+                "SWAP": self.stack.swap,
+                "POP": self.stack.pop,
             },
-            'IO': {
-                'OUTPUT_CHAR': self.io.o_chr,
-                'OUTPUT_NUM': self.io.o_int,
-                'READ_CHAR': partial(self.io.i_chr, self.heap),
-                'READ_NUM': partial(self.io.i_int, self.heap)
+            "IO": {
+                "OUTPUT_CHAR": self.io.o_chr,
+                "OUTPUT_NUM": self.io.o_int,
+                "READ_CHAR": partial(self.io.i_chr, self.heap),
+                "READ_NUM": partial(self.io.i_int, self.heap),
             },
-            'FLOW_CONTROL': {
-                'MARK': (lambda x: None),
-                'CALL': partial(self.call_sub),
-                'JUMP': partial(self.jump_loc),
-                'JUMP_IF_ZERO': partial(self.jump_zero),
-                'JUMP_IF_NEG': partial(self.jump_neg),
-                'END_SUB': partial(self.end_sub),
-                'END': self.end
+            "FLOW_CONTROL": {
+                "MARK": (lambda x: None),
+                "CALL": partial(self.call_sub),
+                "JUMP": partial(self.jump_loc),
+                "JUMP_IF_ZERO": partial(self.jump_zero),
+                "JUMP_IF_NEG": partial(self.jump_neg),
+                "END_SUB": partial(self.end_sub),
+                "END": self.end,
             },
-            'HEAP_ACCESS': {
-                'STORE': partial(self.heap.set),
-                'RETR': partial(self.heap.get)
+            "HEAP_ACCESS": {
+                "STORE": partial(self.heap.set),
+                "RETR": partial(self.heap.get),
             },
-            'ARITHMETIC': {
-                '+': self.stack.math.add,
-                '-': self.stack.math.subtract,
-                '*': self.stack.math.multiply,
-                '/': self.stack.math.divide,
-                '%': self.stack.math.modulo
-            }
+            "ARITHMETIC": {
+                "+": self.stack.math.add,
+                "-": self.stack.math.subtract,
+                "*": self.stack.math.multiply,
+                "/": self.stack.math.divide,
+                "%": self.stack.math.modulo,
+            },
         }
 
     def _get_value(self, ws_int, signed=False):
+        # ws_int is expected to be a string of space/tab characters
+        # For signed integers the first char is the sign bit and the
+        # remainder are the magnitude bits. It's possible the magnitude
+        # is empty (e.g. sign followed immediately by LF), so handle
+        # that case and treat it as zero instead of letting int('', 2)
+        # raise a ValueError.
+        if ws_int is None:
+            return 0
+        sign = ""
         if signed:
-            sign = '-' if NUM_SIGN_CONST[ws_int[0]] == 'NEGATIVE' else ''
+            sign = "-" if NUM_SIGN_CONST.get(ws_int[0]) == "NEGATIVE" else ""
             ws_int = ws_int[1:]
-        number = int(''.join([NUM_CONST[i] for i in ws_int]), 2)
-        return int('{}{}'.format(sign, number)) if signed else number
+        # Map whitespace chars to binary digits, ignoring any unknown
+        # characters defensively by mapping them to empty string.
+        bits = "".join([NUM_CONST.get(i, "") for i in ws_int])
+        number = int(bits, 2) if bits != "" else 0
+        return int(f"{sign}{number}") if signed else number
 
     def create_labels(self):
         labels = dict(
             (
                 (
-                    (self._get_value(t[2].value), idx) for
-                    idx, t in enumerate(self.token_list)
-                    if t[0].type == 'FLOW_CONTROL' and t[1].type == 'MARK'
+                    (self._get_value(t[2].value), idx)
+                    for idx, t in enumerate(self.token_list)
+                    if t[0].type == "FLOW_CONTROL" and t[1].type == "MARK"
                 )
             )
         )
@@ -100,7 +112,7 @@ class Parser(object):
         while self.instruction_ptr < self.num_of_tokens:
             token = self.token_list[self.instruction_ptr]
             if token[1].type in HAS_ARGS:
-                signed = True if token[1].type is 'PUSH' else False
+                signed = True if token[1].type == "PUSH" else False
                 int_value = self._get_value(token[2].value, signed=signed)
                 self.method_map[token[0].type][token[1].type](int_value)
             else:
